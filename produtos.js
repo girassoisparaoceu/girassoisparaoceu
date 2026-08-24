6ee9f2b0e34aded3e972deb7e329a75f20239b46
girassoisparaoceu <girassoisparaoceu@gmail.com>
2026-08-24 19:05:47 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -1,24 +1,4 @@
-/*
-============================================================
-🌻 GIRASSÓIS PARA O CÉU
-CATÁLOGO DE PRODUTOS
-============================================================
-
-Para cadastrar um produto:
-
-nome       → Nome que aparecerá no site
-categoria  → Categoria do produto
-imagem     → Caminho da imagem
-descricao  → Texto curto
-link       → Seu link de afiliado
-
-Não é necessário informar preço.
-============================================================
-*/
-
-
 const produtos = [
-
   {
     nome: "Terço de Madeira para Casal",
     categoria: "Fé & Presentes",
@@ -26,6 +6,4 @@ const produtos = [
     descricao: "Um terço especial para representar a fé, o amor e a caminhada a dois.",
     link: "https://s.shopee.com.br/8AVYRkuleV"
   }
-
-
 ];
