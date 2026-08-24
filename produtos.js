1b270e7e1e36f3e0b79ec8696e68d2aec41ed854
girassoisparaoceu <girassoisparaoceu@gmail.com>
2026-08-24 19:04:00 -0300

    Update produtos.js

--- produtos.js
+++ produtos.js
@@ -22,9 +22,10 @@ const produtos = [
   {
     nome: "Terço de Madeira para Casal",
     categoria: "Fé & Presentes",
-    imagem: "imagens/terco-casal.avif",
+    imagem: "imagens/terco-casal.jpg",
     descricao: "Um terço especial para representar a fé, o amor e a caminhada a dois.",
     link: "https://s.shopee.com.br/8AVYRkuleV"
   }
+
 
 ];
